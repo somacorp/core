a5de76af5f6ca6fa089583453eb89d0e313945f7
somasou <dev@soma.cx>
2023-07-30 05:43:37 +0200

    new package signer

---
+++ signer/signer.go
@@ -0,0 +1,52 @@
+package signer
+
+import (
+	"github.com/somacorp/core/crv"
+	"github.com/somacorp/core/enc"
+)
+
+type Signer struct {
+	privateKey []byte
+	PublicKey  []byte
+	Curve      crv.Curve
+	Encoder    enc.Encoder
+}
+
+func New(privateKey []byte, curve crv.Curve, encoder enc.Encoder) (*Signer, error) {
+	publicKey, err := curve.PublicKey(privateKey)
+	if err != nil {
+		return nil, err
+	}
+	return &Signer{
+		privateKey: privateKey,
+		PublicKey:  publicKey,
+		Curve:      curve,
+		Encoder:    encoder,
+	}, nil
+}
+
+func (s *Signer) PrivateKey() []byte {
+	return s.privateKey
+}
+
+func (s *Signer) ECDH(publicKey []byte) ([]byte, error) {
+	return s.Curve.ECDH(s.privateKey, publicKey)
+}
+
+func (s *Signer) Sign(message string) ([]byte, error) {
+	digest, err := s.Encoder.Encode([]byte(message))
+	if err != nil {
+		return nil, err
+	}
+
+	signature, err := s.Curve.Sign(digest, s.privateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return signature, nil
+}
+
+func (s *Signer) Verify(pubBytes []byte, data []byte, sigBytes []byte) bool {
+	return s.Curve.Verify(pubBytes, data, sigBytes)
+}
